73dff491f9ea34b4068818736b46039d775183b5
amnotpolina <amnotpolina@mail.ru>
2026-04-11 19:54:16 +0500

    fix

--- b2b_frontend/src/lib/components/atoms/MainInfo.svelte
+++ b2b_frontend/src/lib/components/atoms/MainInfo.svelte
@@ -3,5 +3,5 @@
 </script>
 <div class="flex flex-row items-center">
     <img class="size-20 mr-8" src={image} alt={title} />
-    <a href={`my_products/${id}`} class="font-Manrope font-semibold text-2xl duration-100 hover:opacity-80">{title}</a>
+    <a href={`products/${id}`} class="font-Manrope font-semibold text-2xl duration-100 hover:opacity-80">{title}</a>
 </div>
